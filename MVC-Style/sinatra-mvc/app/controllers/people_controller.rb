5fcec366e902c64b13845765cff72f3659c3d7ab
Adria <adriadc@aol.com>
2015-10-30 22:47:25 -0400

    commit updates for crud with forms projects

--- MVC-Style/sinatra-mvc/app/controllers/people_controller.rb
+++ MVC-Style/sinatra-mvc/app/controllers/people_controller.rb
@@ -3,6 +3,22 @@ get '/people' do
   erb :"/people/index"
 end
 
+get '/people/new' do
+    @person = Person.new
+    erb :"/people/new"
+  end  
+
+post '/people' do
+  if params[:birthdate].include?("-")
+      birthdate = params[:birthdate]
+    else
+      birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
+    end
+    
+  person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
+  redirect "/people/#{person.id}"  
+end
+
 get '/people/:id' do
 	@person = Person.find(params[:id])
    birth_path_num = Person.get_birth_path_num(@person.birthdate.strftime("%m%d%Y"))
