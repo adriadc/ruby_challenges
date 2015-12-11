113eb953cad9e56644010b50cb7492ff266b5351
Adria <adriadc@aol.com>
2015-12-11 09:31:46 -0500

    update anagram code

--- Skillcrush_204/annagrams/starter-code/reverse_letters.rb
+++ Skillcrush_204/annagrams/starter-code/reverse_letters.rb
@@ -3,19 +3,19 @@
 #in order to generate an anagram for a two-letter words you just need 
 #to be able to give your method a two letter word and have it return the same letters, but reversed.
 #make an array
-def reverse_letters (strings)
+letters = ["a","b"]
+puts letters.join(", ")
+def reverse_letters(letters)
 #create an array with 2 letters
-	array = strings.length
-	strings = Array.new(2)
+	long = letters.length
+  reversed = Array.new(long)
 
 #Loop/reverse letters in the array
-	array.each_with_index do |strings, index|
-	reverse_letters[length - index - 1] = array	
-  
-end
-#return an array of 2 letters.
-	reverse_letters
- 	
 
+letters.each_with_index do |letter, index| 
+    reversed[long - index - 1] = letter
+  end
+#return an array of 2 letters.
+reversed
 end
-#puts reversed_letters
+puts #{letter}.join(", ") 
