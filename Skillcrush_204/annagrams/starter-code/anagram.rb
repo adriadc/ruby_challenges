ab0a082df8022a270b91fe28beeb83b326ec0c4c
Adria <adriadc@aol.com>
2016-01-14 16:56:56 -0500

    updating master for anagram maker

--- Skillcrush_204/annagrams/starter-code/anagram.rb
+++ Skillcrush_204/annagrams/starter-code/anagram.rb
@@ -1,47 +1,62 @@
 #this is an annagram generator
 require_relative 'reverse_letters'
 
-#require our reverse_letters method
+#require our reverse_letters method file
 #Use the same directory/code structure as you did yesterday — we will reuse our reverse_letters.rb code.
 
 
 #in order to generate an anagram for a three-letter words you just need 
 #to be able to give your method a three letter word and have it return the same letters, but reversed.
 #make an array
-letters = ["abc"]
 
-puts letters
 
-def reverse_letters(letters)
+#have you method accept a three_letter_word
 
-#create an array with 3 letters
-	long = letters.length
-  reversed = Array.new(long)
 
-#Loop/reverse letters in the array
-
-letters.each_with_index do |letter, index| 
-    reversed[long - index - 1] = letter
-  end
-#return an array of 3 letters.
-reversed
-end
-
-puts reverse_letters(letters)
-
-#require our reverse_letters method
-#Use the same directory/code structure as you did yesterday — we will reuse our reverse_letters.rb code.
+ 
 
 #create a method called find anagram
-def find_anagram(word)
-anagram = words.each_with_object(Hash.new []) do |word, hash|
-  hash[word.chars.sort] += [word]
+class Anagram
+	def find_anagram(word)
+		@word = word
+
+
+		#compare anagram 
+		word = 0
+  hash = Hash.new []
+  words.each do |word|
+    word += 1
+    (letters..words.count).each do |i|
+      hash[word.to_s.chars.sort.join] += [word]
+    end
+    hash[word.to_s.chars.sort.join] = hash[word.to_s.chars.sort.join].uniq
+  end
+  return hash
+end
+
+	anagram = Anagram.new
+	anagram.display
+		#If this anagram is a real word then print word
+		#else if 
+		#sort word
+		#compare word to see if a real word
+		#reverse_letters
+		
+		#find_anagram(word)).to be_a(Array)
+
+		
+		#find_anagram(word)).to match_array(
+        #%w(abc acb bac bca cab cba)
+
+
+	
+	puts find_anagram(words)
+
 end
 	
 
 #find_anagram takes a word (string).
+
 #find_anagram returns an array of strings.
 
-end
-
-
+#return the same letters but reversed
--- Skillcrush_204/annagrams/starter-code/reverse_letters.rb
+++ Skillcrush_204/annagrams/starter-code/reverse_letters.rb
@@ -1,22 +1,26 @@
 #in order to generate an anagram for a three-letter words you just need 
 #to be able to give your method a three letter word and have it return the same letters, but reversed.
 #make an array
-letters = ["a","b","c"]
-
-puts letters.join
+#need to get words sorted forwards and backwards at some point
+#%w{apple pear fig}.sort_by { |word| word.length}
+letters = %w{a,b,e}
+puts letters
+#puts letters.join("")
 
 def reverse_letters(letters)
 
 #create an array with 3 letters
 	long = letters.length
   reversed = Array.new(long)
-
+ 
 #Loop/reverse letters in the array
 
-letters.each_with_index do |letter, index| 
+letters.each_with_index do |letter, index|
     reversed[long - index - 1] = letter
-  end
+
+  	end
 #return an array of 3 letters.
-reversed
+	reversed
 end
-puts reverse_letters(letters).join
+puts reverse_letters(letters)
+#puts reverse_letters(letters).join("")
